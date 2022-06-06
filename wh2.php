5d31e3dc3cc96199b60e6df28486efa5c0e5ccf5
pentor4062 <45092353+pentor4062@users.noreply.github.com>
2022-06-06 14:45:06 +0700

    Update wh2.php

--- wh2.php
+++ wh2.php
@@ -4,4 +4,14 @@ $httpRequestBody = 'test'; // Request body string
 $hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
 $signature = base64_encode($hash);
 // Compare x-line-signature request header string and the signature
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('F0oLgnfOnTKNmceloGRmDAIErcJKBQZKEiYXMHbdhqwsUlZiDD38TivpbCTdN0jQuAoIxho1EMxcTUmEx2qMs8eOmMUad+/D/Bvihu+3SPNDNCY9d3GLqxAxQk+uV7SXGUPa1I1hMgNUeNhpMPencAdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'd04163fe826d2617bad0770ddc99478f']);
+$response = $bot->getMessageContent('@560ttqbe');
+if ($response->isSucceeded()) {
+    $tempfile = tmpfile();
+    fwrite($tempfile, $response->getRawBody());
+} else {
+    error_log($response->getHTTPStatus() . ' ' . $response->getRawBody());
+}
 ?>
