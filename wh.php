33890fa49166bd24a4a226e36d4abec54b340b03
pentor4062 <45092353+pentor4062@users.noreply.github.com>
2022-06-06 14:28:21 +0700

    Update wh.php

--- wh.php
+++ wh.php
@@ -1,7 +1,7 @@
 <?php
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<channel access token>');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<channel secret>']);
-$response = $bot->getMessageContent('<messageId>');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('F0oLgnfOnTKNmceloGRmDAIErcJKBQZKEiYXMHbdhqwsUlZiDD38TivpbCTdN0jQuAoIxho1EMxcTUmEx2qMs8eOmMUad+/D/Bvihu+3SPNDNCY9d3GLqxAxQk+uV7SXGUPa1I1hMgNUeNhpMPencAdB04t89/1O/w1cDnyilFU=');
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'd04163fe826d2617bad0770ddc99478f']);
+$response = $bot->getMessageContent('U920f3957cf4902cfb155d6bb0d3a2a68');
 if ($response->isSucceeded()) {
     $tempfile = tmpfile();
     fwrite($tempfile, $response->getRawBody());
