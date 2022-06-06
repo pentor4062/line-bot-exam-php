332753d676ed921d4203fd41749bbbf24651f2e9
pentor4062 <45092353+pentor4062@users.noreply.github.com>
2022-06-06 14:16:35 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,8 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
-
+$access_token = 'F0oLgnfOnTKNmceloGRmDAIErcJKBQZKEiYXMHbdhqwsUlZiDD38TivpbCTdN0jQuAoIxho1EMxcTUmEx2qMs8eOmMUad+/D/Bvihu+3SPNDNCY9d3GLqxAxQk+uV7SXGUPa1I1hMgNUeNhpMPencAdB04t89/1O/w1cDnyilFU='
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
