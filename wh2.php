b537e707443f9166732bca37132b002c12c66a3f
pentor4062 <45092353+pentor4062@users.noreply.github.com>
2022-06-06 14:47:25 +0700

    Update wh2.php

--- wh2.php
+++ wh2.php
@@ -1,8 +1,8 @@
 <?php
-$channelSecret = 'd04163fe826d2617bad0770ddc99478f'; // Channel secret string
-$httpRequestBody = 'test'; // Request body string
-$hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
-$signature = base64_encode($hash);
+// $channelSecret = 'd04163fe826d2617bad0770ddc99478f'; // Channel secret string
+// $httpRequestBody = 'test'; // Request body string
+// $hash = hash_hmac('sha256', $httpRequestBody, $channelSecret, true);
+// $signature = base64_encode($hash);
 // Compare x-line-signature request header string and the signature
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('F0oLgnfOnTKNmceloGRmDAIErcJKBQZKEiYXMHbdhqwsUlZiDD38TivpbCTdN0jQuAoIxho1EMxcTUmEx2qMs8eOmMUad+/D/Bvihu+3SPNDNCY9d3GLqxAxQk+uV7SXGUPa1I1hMgNUeNhpMPencAdB04t89/1O/w1cDnyilFU=');
